4d854192daf2fd0b15d50aaebf418d9d11150c7d
zhangfuqiang <ivesein@outlook.com>
2022-07-06 16:31:07 +0800

    update

--- src/api/inspectApi.js
+++
@@ -1,9 +0,0 @@
-import service from "@/clean-data/service"; //调用公路云客户端接口
-import xApi from "@/clean-data/xApi"; //调用中台接口
-import { INIT_DEMO_DATA } from "./urlConfig";
-
-// 例子
-export const getDemoData = (params) => {
-	return service.get(INIT_DEMO_DATA);
-};
-
